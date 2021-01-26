2e0a30c733b59b324b55bf6714aa42ff0cd02a31
Anssi Myllymäki <anssi@pihlasoft.com>
2021-01-26 10:18:10 +0200

    test

--- db/index.js
+++ db/index.js
@@ -1,25 +1,19 @@
 const { Pool } = require('pg')
 
-let connectionInfo = {}
 
-//process.env.HEROKU
 
-if (true) {
-  connectionInfo = process.env.DATABASE_URL
-}
-else {
-  connectionInfo =
-  {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Tenttikanta',
-    password: 'admin',
-    port: 5432,
-  }
+const pool = new Pool({
 
-}
-
-const pool = new Pool(connectionInfo)
+  connectionString: process.env.DATABASE_URL
+  
+  /*
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Tenttikanta',
+  password: 'admin',
+  port: 5432,
+  */
+})
 
 module.exports = {
   query: (text, params, callback) => {
