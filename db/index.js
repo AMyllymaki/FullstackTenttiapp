bb0569081031bf5ffcf156d414c253d2ed5b61a5
Anssi Myllymäki <anssi@pihlasoft.com>
2021-02-10 11:51:33 +0200

    added some console.logs

--- db/index.js
+++ db/index.js
@@ -3,13 +3,16 @@ const { Pool } = require('pg')
 let pool
 
 if (process.env.HEROKU) {
+  console.log("Process is HEROKU")
   pool = new Pool({ connectionString: process.env.DATABASE_URL })
 }
 else if(process.env.AZURE)
 {
+  console.log("Process is AZURE")
   pool = new Pool({ connectionString: process.env.CONNECTION_STRING })
 }
 else {
+  console.log("Process is LOCAL")
 
   pool = new Pool({
 
