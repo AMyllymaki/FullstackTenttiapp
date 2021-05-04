9dc8a305c9cedeeb2f712819e532e4de872d0b5c
Anssi Myllymäki <anssi@pihlasoft.com>
2021-05-04 10:06:37 +0300

    Heroku Setup

--- client/src/ServerSettings.js
+++ client/src/ServerSettings.js
@@ -4,7 +4,7 @@ const getEnvironment = () =>
 {
     switch (process.env.NODE_ENV) {
         case 'production':
-            return 'https://fullstacktenttiapp.azurewebsites.net'
+            return 'https://tentti-app.herokuapp.com'
         case 'development':
             return 'http://localhost:4000'
         case 'test':
